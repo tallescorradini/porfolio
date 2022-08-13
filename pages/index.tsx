28dc4888c05fee818eb6e959622d231281aa0ae5
Talles Corradini <tallescorradini@gmail.com>
2022-08-13 16:58:45 -0300

    feat: add page title and description

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,18 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 
 const Home: NextPage = () => {
-  return <></>;
+  return (
+    <>
+      <Head>
+        <title>Talles C. Corradini</title>
+        <meta
+          name="description"
+          content="Conheça uma seleção de projetos web que desenvolvi"
+        />
+      </Head>
+    </>
+  );
 };
 
 export default Home;
