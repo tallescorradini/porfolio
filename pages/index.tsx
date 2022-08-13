e8f84551acb6d2da9a6f6279390031dc3d527c08
Talles Corradini <tallescorradini@gmail.com>
2022-08-13 17:25:21 -0300

    feat: add page favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,6 +10,31 @@ const Home: NextPage = () => {
           name="description"
           content="Conheça uma seleção de projetos web que desenvolvi"
         />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/favicon/safari-pinned-tab.svg"
+          color="#635298"
+        />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff"></meta>
       </Head>
     </>
   );
